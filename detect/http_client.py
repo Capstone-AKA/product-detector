bdf369d34822db37ce0e99233ae825f2aef0d25f
donggun <donggunmaru@gmail.com>
2025-10-29 20:28:51 +0900

    Feat: Update HTTP client

--- detect/http_client.py
+++ detect/http_client.py
@@ -4,7 +4,7 @@ class HttpPostClient:
     def __init__(self, base_url):
         self.base_url = base_url
 
-    def post_json(self, endpoint, data, headers=None):
+    def post_json(self, data, endpoint, headers=None):
         url = self.base_url + endpoint
         try:
             response = requests.post(url, json=data, headers=headers)
