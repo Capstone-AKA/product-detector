ba45bacbac0e05a3b2c9b93d61873c79ba8a713c
donggun <donggunmaru@gmail.com>
2025-10-29 20:29:20 +0900

    Feat: Apply HTTP request client

--- detect/detect.py
+++ detect/detect.py
@@ -1,12 +1,14 @@
 from ultralytics import YOLO
 import cv2
 
-from config_env import PD, HOST_URL, DEBUG
+from config_env import PD, CART_ID, HOST_URL, ADD_ENDPOINT, DEBUG
 from product_tracker import ProductTracker
+from http_client import HttpPostClient
 
 # 1) Load
 model = YOLO(PD)
 pt = ProductTracker()
+hpc = HttpPostClient(HOST_URL)
 
 # 2) Open and set webcam.
 cap = cv2.VideoCapture(0)
@@ -27,11 +29,19 @@ try:
             imgsz=640,
             conf=0.25,
             device=0,
-            verbose=false,
+            verbose=False,
         )
         
         # 4) Decide whether add product to cart.
         add_list = pt.track_product(results)
+
+        # 5) Send added product list to server.
+        if add_list:
+            data = {
+                'cart_number': CART_ID,
+                'product_list': add_list
+            }
+            hpc.post_json(data, ADD_ENDPOINT)
         
 except KeyboardInterrupt:
     pass
