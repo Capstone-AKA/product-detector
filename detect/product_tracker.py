38f0443cf7e3c0437118b93e0a62e06284a995e3
donggun <donggunmaru@gmail.com>
2025-11-13 14:22:26 +0900

    Feat: Convert Track from list to class

--- detect/product_tracker.py
+++ detect/product_tracker.py
@@ -1,6 +1,24 @@
 import numpy as np
 from config_env import DEBUG
 
+class Track:
+    __slots__ = ("name", "location", "count", "update_flag")
+    
+    def __init__(self, name, location, count=1):
+        self.name = name
+        self.location = location
+        self.count = count
+        self.update_flag = True
+
+    def reset_update_flag(self):
+        self.update_flag = False
+
+    def update(self, location):
+        self.location = location
+        self.count += 1
+        self.update_flag = True
+
+
 class ProductTracker:
     def __init__(self, iou_threshold=0.6, count_threshold=5, min_area_norm=0.4):
         self.iou_threshold = float(iou_threshold)
@@ -10,16 +28,16 @@ class ProductTracker:
 
     def _compute_iou(self, boxA, boxB):
         # box: [x1,y1,x2,y2]
-        xA = max(float(boxA[0]), float(boxB[0]))
-        yA = max(float(boxA[1]), float(boxB[1]))
-        xB = min(float(boxA[2]), float(boxB[2]))
-        yB = min(float(boxA[3]), float(boxB[3]))
+        xA = max(boxA[0], boxB[0])
+        yA = max(boxA[1], boxB[1])
+        xB = min(boxA[2], boxB[2])
+        yB = min(boxA[3], boxB[3])
 
         interW = max(0.0, xB - xA)
         interH = max(0.0, yB - yA)
         interArea = interW * interH
-        areaA = max(0.0, (float(boxA[2]) - float(boxA[0]))) * max(0.0, (float(boxA[3]) - float(boxA[1])))
-        areaB = max(0.0, (float(boxB[2]) - float(boxB[0]))) * max(0.0, (float(boxB[3]) - float(boxB[1])))
+        areaA = max(0.0, (boxA[2] - boxA[0])) * max(0.0, (boxA[3] - boxA[1]))
+        areaB = max(0.0, (boxB[2] - boxB[0])) * max(0.0, (boxB[3] - boxB[1]))
         denom = areaA + areaB - interArea + 1e-6
         return interArea / denom
 
@@ -56,50 +74,46 @@ class ProductTracker:
         detections: Ultralytics results list
         return: List of product names confirmed to be added (Product reached 'count == count_threshold')
         """
-        new_track_list = []
         add_list = []
 
         products = self._filter_detection(detections)
+
+        for track in self.track_list:
+            track.reset_update_flag()
 
         for prd in products:
             xyxy = prd['xyxy']
             name = prd['name']
             best_iou = 0
-            best_id = None
+            best_track = None
 
             # Match with existing track
-            for idx, track in enumerate(self.track_list):
-                iou = self._compute_iou(xyxy, track['location'])
-                if iou > best_iou:
+            for track in self.track_list:
+                iou = self._compute_iou(xyxy, track.location)
+                if iou>=self.iou_threshold and iou>best_iou:
                     best_iou = iou
-                    best_id = idx
+                    best_track = track
 
             # Update existing track
-            if best_iou > self.iou_threshold:
-                track = self.track_list[best_id]
-                track['location'] = xyxy
-                track['count'] += 1
-                new_track_list.append(track)
+            if best_track:
+                best_track.update(xyxy)
                 
-                if track['count'] == self.count_threshold:
+                if best_track.count == self.count_threshold:
                     add_list.append(name)
                     if DEBUG:
                         print(f'{name} added to cart.')
 
             # Create new track
             else:
-                new_track_list.append({
-                    'name': name,
-                    'location': xyxy,
-                    'count': 1
-                })
+                self.track_list.append(Track(name, xyxy))
 
-        self.track_list = new_track_list
+        # Remove not updated tracks.
+        self.track_list = [t for t in self.track_list if t.update_flag]
 
         if DEBUG and len(self.track_list) > 0:
             log='tracker list: '
             for track in self.track_list:
-                log += f"{track['name']}({track['count']}), "
+                log += f"{track.name}({track.count}), "
             print(log)
         
         return add_list
